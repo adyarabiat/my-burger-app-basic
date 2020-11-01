e2f333f852522c5c570198365d4e57be21c02d0f
adyarabiat <ady.arabiat@gmail.com>
2020-11-01 23:51:36 +0400

    using shouldComponent update in the Model to improve the performance commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = (props) => {
-  const ingredientsSummary = Object.keys(props.ingredients).map((key) => {
-    return (
-      <li key={key}>
-        <span style={{ textTransform: "capitalize" }}>{key}</span>:
-        {props.ingredients[key]}
-      </li>
+// We change it to class based component to check the performance
+// So to do so
+// First we add componentDidUpdate to check when it is updating
+class OrderSummary extends Component {
+  componentDidUpdate() {
+    console.log("[orderSummary.js componentDidUpdate ]");
+  }
+  render() {
+    const ingredientsSummary = Object.keys(this.props.ingredients).map(
+      (key) => {
+        return (
+          <li key={key}>
+            <span style={{ textTransform: "capitalize" }}>{key}</span>:
+            {this.props.ingredients[key]}
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A deliciouse burger with the following ingredients:</p>
-      <ul>{ingredientsSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)} $</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="Success" click={props.continue}>
-        Continue
-      </Button>
-      <Button btnType="Danger" click={props.cancel}>
-        Cancel
-      </Button>
-    </Aux>
-  );
-};
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A deliciouse burger with the following ingredients:</p>
+        <ul>{ingredientsSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)} $</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="Success" click={this.props.continue}>
+          Continue
+        </Button>
+        <Button btnType="Danger" click={this.props.cancel}>
+          Cancel
+        </Button>
+      </Aux>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
 
 // importing orderSummary in the Burger builder
--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,24 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
 import styles from "./Model.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux";
 
-const model = (props) => {
-  return (
-    <Aux>
-      <Backdrop show={props.checkShow} click={props.modelClose} />
-      <div
-        className={styles.Modal}
-        style={{
-          transform: props.checkShow ? "translateY(0)" : "translateY(-100vh)",
-          opacity: props.checkShow ? "1" : "0",
-        }}
-      >
-        {props.children}
-      </div>
-    </Aux>
-  );
-};
+class Model extends Component {
+  // To improve the performance we should not update the Model everytime we change something in the UI
 
-export default model;
+  // So becouse of this we use shouldComponentUpdate
+  // So it will check if the props.checkShow !== to the current props.checkShow So by that it means that we clicked on it and it will show so it check here always if the next one not the same as the prev one
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.checkShow !== this.props.checkShow;
+  }
+
+  componentDidUpdate() {
+    console.log("[Model.js Update]");
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop show={this.props.checkShow} click={this.props.modelClose} />
+        <div
+          className={styles.Modal}
+          style={{
+            transform: this.props.checkShow
+              ? "translateY(0)"
+              : "translateY(-100vh)",
+            opacity: this.props.checkShow ? "1" : "0",
+          }}
+        >
+          {this.props.children}
+        </div>
+      </Aux>
+    );
+  }
+}
+
+export default Model;
