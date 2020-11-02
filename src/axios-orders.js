efad3773f515c9389ead932fe954c87f203a2cab
adyarabiat <ady.arabiat@gmail.com>
2020-11-02 22:57:37 +0400

    the same as the prev commit just save

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axiosInstance from "axios";
 
-const instance = axios.create({
+const instance = axiosInstance.create({
   baseURL: "https://react-my-burger-65308.firebaseio.com/",
 });
 
