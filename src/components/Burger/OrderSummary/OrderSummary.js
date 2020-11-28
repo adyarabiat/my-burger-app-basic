b1282f4d98c37b9db557df706203c5fbd70de2a4
adyarabiat <ady.arabiat@gmail.com>
2020-11-28 16:41:22 +0400

    Changing more components to React Hooks , in The model we change to react hooks but we use React.memo to optimize the performance commit

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -6,42 +6,36 @@ import Button from "../../UI/Button/Button";
 // We change it to class based component to check the performance
 // So to do so
 // First we add componentDidUpdate to check when it is updating
-class OrderSummary extends Component {
-  componentDidUpdate() {
-    // console.log("[orderSummary.js componentDidUpdate ]");
-  }
-  render() {
-    const ingredientsSummary = Object.keys(this.props.ingredients).map(
-      (key) => {
-        return (
-          <li key={key}>
-            <span style={{ textTransform: "capitalize" }}>{key}</span>:
-            {this.props.ingredients[key]}
-          </li>
-        );
-      }
-    );
+
+const OrderSummary = (props) => {
+  const ingredientsSummary = Object.keys(props.ingredients).map((key) => {
     return (
-      <Aux>
-        <h3>Your Order</h3>
-        <p>A deliciouse burger with the following ingredients:</p>
-        <ul>{ingredientsSummary}</ul>
-        <p>
-          <strong>Total Price: {this.props.price.toFixed(2)} $</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-
-        <Button btnType="Success" click={this.props.continue}>
-          Continue
-        </Button>
-
-        <Button btnType="Danger" click={this.props.cancel}>
-          Cancel
-        </Button>
-      </Aux>
+      <li key={key}>
+        <span style={{ textTransform: "capitalize" }}>{key}</span>:
+        {props.ingredients[key]}
+      </li>
     );
-  }
-}
+  });
+  return (
+    <Aux>
+      <h3>Your Order</h3>
+      <p>A deliciouse burger with the following ingredients:</p>
+      <ul>{ingredientsSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)} $</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+
+      <Button btnType="Success" click={props.continue}>
+        Continue
+      </Button>
+
+      <Button btnType="Danger" click={props.cancel}>
+        Cancel
+      </Button>
+    </Aux>
+  );
+};
 
 export default OrderSummary;
 
--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -4,36 +4,45 @@ import styles from "./Model.module.css";
 import Backdrop from "../Backdrop/Backdrop";
 import Aux from "../../../hoc/Aux/Aux";
 
-class Model extends Component {
+const Model = (props) => {
   // To improve the performance we should not update the Model everytime we change something in the UI
 
   // So becouse of this we use shouldComponentUpdate
   // So it will check if the props.checkShow !== to the current props.checkShow So by that it means that we clicked on it and it will show so it check here always if the next one not the same as the prev one
-  shouldComponentUpdate(nextProps, nextState) {
-    return (
-      nextProps.checkShow !== this.props.checkShow ||
-      nextProps.children !== this.props.children
-    );
-  }
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   return (
+  //     nextProps.checkShow !== this.props.checkShow ||
+  //     nextProps.children !== this.props.children
+  //   );
+  // }
 
-  render() {
-    return (
-      <Aux>
-        <Backdrop show={this.props.checkShow} click={this.props.modelClose} />
-        <div
-          className={styles.Modal}
-          style={{
-            transform: this.props.checkShow
-              ? "translateY(0)"
-              : "translateY(-100vh)",
-            opacity: this.props.checkShow ? "1" : "0",
-          }}
-        >
-          {this.props.children}
-        </div>
-      </Aux>
-    );
-  }
-}
+  return (
+    <Aux>
+      <Backdrop show={props.checkShow} click={props.modelClose} />
+      <div
+        className={styles.Modal}
+        style={{
+          transform: props.checkShow ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.checkShow ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </Aux>
+  );
+};
 
-export default Model;
+// React.memo :
+
+// React memo it allows us to optimize performance and only update the component when the props are component change.
+
+// And it give us a second arrgument that we can pass to give our logic when to update
+
+// Here we say check if that is true so it is the opposite to the one we use in the shouldComponentUpdate
+
+export default React.memo(
+  Model,
+  (prevProps, nextProps) =>
+    nextProps.show === prevProps.show &&
+    nextProps.children === prevProps.children
+);
